c3d2e7d0bb210a00cb0b5026d8d99ae0640ca3e4
Snehal.kale <Snehal.kale@ESPLL076.Eternus.local>
2020-09-03 14:44:04 +0530

    image resizing

--- final code/routes/user/index.js
+++ final code/routes/user/index.js
@@ -1,23 +1,42 @@
-var express = require('express');
-var router = express.Router();
-var assert = require('assert');
+const express = require('express');
+const router = express();
+const path = require('path');
+const multer = require('multer');
 const sharp = require('sharp');
- 
 
-var multer = require('multer');
-var storage = multer.diskStorage({
+const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, 'public/images/uploads')
+        cb(null, 'public/images/uploads');
     },
     filename: (req, file, cb) => {
-      cb(null, file.fieldname + '-' + Date.now()+ ".jpg")
+        console.log(file);
+        cb(null, Date.now() + path.extname(file.originalname));
     }
 });
-var upload = multer({storage: storage});
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+const upload = multer({ storage: storage, fileFilter: fileFilter });
 
-
-router.post('/fileUpload', upload.single('image'), async(req, res, next) => {
-  res.json({'message': 'File uploaded successfully'});
+router.post('/upload', upload.single('image'), (req, res, next) => {
+    try {
+        sharp(req.file.path).resize(200, 200).toFile('public/images/uploads/' + 'thumbnails-' + req.file.originalname, (err, resizeImage) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(resizeImage);
+            }
+        })
+        return res.status(201).json({
+            message: 'File uploded successfully'
+        });
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 module.exports = router;
